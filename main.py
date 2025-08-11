8bd3ad1c4ee151bd4726fd31a9a84f5fab41181c
Filip Gregor <filip.gregor@gmail.com>
2025-08-11 18:38:49 +0200

    working promot request from console

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+import sys
+
+def main():
+    print("Hello from ai-agent!")
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = sys.argv[1]
+
+    verbose = "--verbose" in sys.argv
+
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+
+    model = "gemini-2.0-flash-001"
+
+    response = client.models.generate_content(model = model, contents = messages)
+
+    print(response.text)
+
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
