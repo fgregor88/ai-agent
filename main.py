d64620eb8a80a013782ab7207881e33847b1f9cb
Filip Gregor <filip.gregor@gmail.com>
2025-08-17 11:09:45 +0200

    hacks

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ def main():
     When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
     - List files and directories
     All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    You are inside calculator app directory.
     """
 
     for i in range(20): 
